d948b2cf456b782bda9ba61f2ac7d27a39cdf382
Mapaaachito <83160603+Mapachey@users.noreply.github.com>
2022-05-15 18:04:39 +0200

    Add files via upload

---
+++ mapachito_carhud/fxmanifest.lua
@@ -0,0 +1,22 @@
+-- FX Info
+fx_version 'adamant'
+game 'gta5'
+
+-- Credits
+author 'Mapachit0_#9198'
+description 'Hud simple disfrutalo :)'
+
+-- Resources
+ui_page 'html/index.html'
+
+files {
+    "html/index.html",
+    "html/styles.css",
+    "html/BoxedRound.ttf",
+    "html/main.js"
+}
+
+client_scripts {
+    "config.lua",
+    "code/main.lua"
+}
